23860311c25f8cf03fd5424c1c70266bc2d49289
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-01 22:37:14 +0300

    Create news_adapter_activity layout to hold base adapter data that will be added to the listView

--- app/src/main/java/com/moringaschool/hellozuz/NewsFeedsActivity.java
+++ app/src/main/java/com/moringaschool/hellozuz/NewsFeedsActivity.java
@@ -3,12 +3,17 @@ package com.moringaschool.hellozuz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import butterknife.BindView;
 
 public class NewsFeedsActivity extends AppCompatActivity {
+    @BindView(R.id.newsList) ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_feeds2);
+
     }
 }
