a73d0f8ff8abb355bfe253a34a938fa6cadcefd2
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-01 20:44:03 +0300

    Add NewsFeedsActivity, fetch login button and username views by their ids, fetch the userName text, set an onClick listener on the login button and create an intent to start NewsFeedActivity when it is clicked, pass in fetched userName to the intent

--- app/src/main/java/com/moringaschool/hellozuz/MainActivity.java
+++ app/src/main/java/com/moringaschool/hellozuz/MainActivity.java
@@ -2,13 +2,36 @@ package com.moringaschool.hellozuz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.userName) EditText mUserName;
+    @BindView(R.id.loginButton) Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mLoginButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == mLoginButton) {
+            String userName = mUserName.getText().toString();
+            Intent intent = new Intent (MainActivity.this, NewsFeedsActivity.class);
+            intent.putExtra("user", userName);
+            startActivity(intent);
+        }
     }
 }
