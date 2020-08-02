c7c22df8a80de660d9b96787070ae88b3cafe89e
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-02 18:49:48 +0300

    Add messageAdapter class that is customised to display individual message object

---
+++ app/src/main/java/com/moringaschool/hellozuz/MessageAdapter.java
@@ -0,0 +1,61 @@
+package com.moringaschool.hellozuz;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MessageAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<MessageData> mMessages;
+    public MessageAdapter (Context context, List<MessageData> messages){
+        this.mContext = context;
+        this.mMessages = messages;
+    }
+    @Override
+    public int getCount() {
+        return mMessages.size();
+    }
+
+    @Override
+    public MessageData getItem(int i) {
+        return mMessages.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        MessageAdapter.MyViewHolder mViewHolder;
+        if (convertView == null) {
+            // get layout from xml file
+            convertView = inflater.inflate(R.layout.message_adapter_activity, null);
+            mViewHolder = new MessageAdapter.MyViewHolder(convertView);
+            convertView.setTag(mViewHolder);
+
+        } else {
+            mViewHolder = (MessageAdapter.MyViewHolder) convertView.getTag();
+        }
+        mViewHolder.messageView.setText(mMessages.get(position).getMessage());
+        mViewHolder.nameView.setText(mMessages.get(position).getSender());
+        return convertView;
+    }
+    private class MyViewHolder {
+        TextView nameView, messageView;
+
+        public MyViewHolder(View item) {
+            nameView = (TextView) item.findViewById(R.id.senderName);
+            messageView = (TextView) item.findViewById(R.id.messageContent);
+        }
+    }
+}
+
