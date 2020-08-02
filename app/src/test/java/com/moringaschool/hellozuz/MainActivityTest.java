d0caaf4b603913b702bcf798f3192ec005f97166
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-02 22:44:28 +0300

    Add MainActivityTest class for testing mainActivity class behaviours

---
+++ app/src/test/java/com/moringaschool/hellozuz/MainActivityTest.java
@@ -0,0 +1,42 @@
+package com.moringaschool.hellozuz;
+
+import android.content.Intent;
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = {Build.VERSION_CODES.O_MR1})
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent(){
+        TextView aboutHeader = activity.findViewById(R.id.aboutHeader);
+        assertTrue("WELCOME TO helloZuz!".equals(aboutHeader.getText().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted(){
+        activity.findViewById(R.id.loginButton).performClick();
+        Intent expectedIntent = new Intent(activity, NewsFeedsActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+}
