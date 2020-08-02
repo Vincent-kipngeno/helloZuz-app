d633a2769ce33532b0839bdcfdbd584a41ecc2d6
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-02 17:52:50 +0300

    Create MessageData class that will contain the contents of message object

---
+++ app/src/main/java/com/moringaschool/hellozuz/MessageData.java
@@ -0,0 +1,27 @@
+package com.moringaschool.hellozuz;
+
+public class MessageData {
+    private String message;
+    private String sender;
+
+    public MessageData(String message, String sender) {
+        this.message = message;
+        this.sender = sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
