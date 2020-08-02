9c582670635fb5644eb3b256d9b595eb334d63df
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-02 23:17:44 +0300

    Add MessageListActivityTest class for testing MessageListActivity class behaviours

---
+++ app/src/test/java/com/moringaschool/hellozuz/MessageListActivityTest.java
@@ -0,0 +1,31 @@
+package com.moringaschool.hellozuz;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = {Build.VERSION_CODES.O_MR1})
+public class MessageListActivityTest {
+    private MessageListActivity activity;
+
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(MessageListActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent(){
+        TextView pageHeader = activity.findViewById(R.id.pageHeader);
+        assertTrue("MESSAGES".equals(pageHeader.getText().toString()));
+    }
+}
