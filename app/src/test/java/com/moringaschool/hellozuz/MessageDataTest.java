b8bb378255911ab533060a7ee4a70d937b3450fc
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-02 23:36:35 +0300

    Create MessageDataTest class to test behaviours of MessageData class

---
+++ app/src/test/java/com/moringaschool/hellozuz/MessageDataTest.java
@@ -0,0 +1,39 @@
+package com.moringaschool.hellozuz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageDataTest {
+
+    @Test
+    public void getMessage() {
+        MessageData messageData = setMessageData();
+        assertEquals("Hey come over here", messageData.getMessage());
+    }
+
+    @Test
+    public void setMessage() {
+        MessageData messageData = setMessageData();
+        messageData.setMessage("Hey stop it");
+        assertEquals("Hey stop it", messageData.getMessage());
+    }
+
+    @Test
+    public void getSender() {
+        MessageData messageData = setMessageData();
+        assertEquals("Monsoon", messageData.getSender());
+    }
+
+    @Test
+    public void setSender() {
+        MessageData messageData = setMessageData();
+        messageData.setSender("Koech");
+        assertEquals("Koech", messageData.getSender());
+    }
+
+    //helpers
+    public MessageData setMessageData() {
+        return new MessageData("Hey come over here", "Monsoon");
+    }
+}
