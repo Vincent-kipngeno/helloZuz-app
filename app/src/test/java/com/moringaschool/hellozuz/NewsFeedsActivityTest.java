2399dcfefbb9b895002123a2444b2e9890a28178
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-02 23:09:43 +0300

    Add NewsFeedsActivityTest class for testing NewsFeedsActivity class behaviours

---
+++ app/src/test/java/com/moringaschool/hellozuz/NewsFeedsActivityTest.java
@@ -0,0 +1,36 @@
+package com.moringaschool.hellozuz;
+
+import android.content.Intent;
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = {Build.VERSION_CODES.O_MR1})
+public class NewsFeedsActivityTest {
+    private NewsFeedsActivity activity;
+
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(NewsFeedsActivity.class);
+    }
+
+    @Test
+    public void secondActivityStarted(){
+        activity.findViewById(R.id.directMessages).performClick();
+        Intent expectedIntent = new Intent(activity, MessageListActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+}
