e66c511497b7edcdd1f9b0a5cfcecf7dcd16f8e5
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-10 18:47:00 +0300

    Add PhotoDetailsActivity and PhotoDetailsFragment

---
+++ app/src/main/java/com/moringaschool/hellozuz/ui/PhotoDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.moringaschool.hellozuz.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.moringaschool.hellozuz.R;
+
+public class PhotoDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_details);
+    }
+}
