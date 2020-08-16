fb599c424b1f18b5a754b5de0e3091f532d4132a
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-17 00:46:37 +0300

    Add registration functionality by email to register activity

--- app/src/main/java/com/moringaschool/hellozuz/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/hellozuz/ui/LoginActivity.java
@@ -1,16 +1,171 @@
 package com.moringaschool.hellozuz.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.moringaschool.hellozuz.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener
+{
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
+
+    @BindView(R.id.login_button) Button LoginButton;
+    @BindView(R.id.phone_login_button) Button PhoneLoginButton;
+    @BindView(R.id.login_email) EditText UserEmail;
+    @BindView(R.id.login_password) EditText UserPassword;
+    @BindView(R.id.need_new_account_link) TextView NeedNewAccountLink;
+    @BindView(R.id.forget_password_link) TextView ForgetPasswordLink;
+
+    private DatabaseReference UsersRef;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        ButterKnife.bind(this);
+        loadingBar = new ProgressDialog(this);
+
+
+        NeedNewAccountLink.setOnClickListener(this);
+
+        LoginButton.setOnClickListener(this);
+
+        PhoneLoginButton.setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == NeedNewAccountLink) {
+            SendUserToRegisterActivity();
+        }
+        if (view == LoginButton) {
+            AllowUserToLogin();
+        }
+        if (view == PhoneLoginButton) {
+            Intent phoneLoginIntent = new Intent(LoginActivity.this, PhoneLoginActivity.class);
+            startActivity(phoneLoginIntent);
+        }
+    }
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null)
+        {
+            SendUserToDashboardActivity();
+        }
+    }
+
+
+
+    private void AllowUserToLogin()
+    {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT).show();
+            UserEmail.setError("Please enter email...");
+        }
+        if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT).show();
+            UserPassword.setError("Please enter password...");
+        }
+        else
+        {
+            loadingBar.setTitle("Sign In");
+            loadingBar.setMessage("Please wait....");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if (task.isSuccessful())
+                            {
+                                String currentUserId = mAuth.getCurrentUser().getUid();
+                                String deviceToken = FirebaseInstanceId.getInstance().getToken();
+
+                                UsersRef.child(currentUserId).child("device_token")
+                                        .setValue(deviceToken)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task)
+                                            {
+                                                if (task.isSuccessful())
+                                                {
+                                                    SendUserToDashboardActivity();
+                                                    Toast.makeText(LoginActivity.this, "Logged in Successful...", Toast.LENGTH_SHORT).show();
+                                                    loadingBar.dismiss();
+                                                }
+                                            }
+                                        });
+                            }
+                            else
+                            {
+                                String message = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+
+
+
+
+
+
+    private void SendUserToDashboardActivity()
+    {
+        Intent mainIntent = new Intent(LoginActivity.this, DashboardActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    private void SendUserToRegisterActivity()
+    {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
+    }
+
 }
