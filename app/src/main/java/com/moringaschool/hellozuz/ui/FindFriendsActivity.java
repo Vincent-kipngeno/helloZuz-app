035033244a4a50b20d76a4dde84187a9f5048041
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-17 02:29:25 +0300

    Create main menu options in menu directory, create FindFriendsActivity

---
+++ app/src/main/java/com/moringaschool/hellozuz/ui/FindFriendsActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.hellozuz.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.hellozuz.R;
+
+public class FindFriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_friends);
+    }
+}
