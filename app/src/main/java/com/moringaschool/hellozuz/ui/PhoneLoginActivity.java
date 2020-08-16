31d5259edc562f2feee8169ef04a87ce9b58124e
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-08-16 20:58:14 +0300

    Add email login and register activity and phone login activity

---
+++ app/src/main/java/com/moringaschool/hellozuz/ui/PhoneLoginActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.hellozuz.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.hellozuz.R;
+
+public class PhoneLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_login);
+    }
+}
